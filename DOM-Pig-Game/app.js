ad1855cddc3ed54876d24ee1f244b8d80f0dcb38
= <jessur2020@gmail.com>
2020-07-02 01:48:59 -0400

    Final commit. Dice game ready to 🚀🚀🚀

--- DOM-Pig-Game/app.js
+++ DOM-Pig-Game/app.js
@@ -1,16 +1,15 @@
 /*********************************
- * *Pig game Rules:
+ * *Dice game Rules:
  */
 
 // let score = [0,0]
 // let roundScore = 0;
 
-//Variables declarations here
-let scores, roundScore, activePlayer;
+//*******Global Variables******************//
+let scores, roundScore, activePlayer, gamePlay;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+//this call back the initFunction
+initFunction();
 
 // document.querySelector("#current-" + activePlayer).textContent = dice;
 // // document.querySelector("#current-" + activePlayer).innerHTML =
@@ -23,10 +22,6 @@ document.querySelector(".dice").style.display = "none";
 
 //document.getElementById allows you to manipulate by their Ids
 //Which is faster than querySelector
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
 
 // // This is an example of a CALL BACK FUNCTION
 // function btn (){
@@ -41,51 +36,59 @@ document.getElementById("current-1").textContent = "0";
 //: this function doesn't have a name. There for it cant'not be reause
 //
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  //1. ramdon number
-  let dice = Math.floor(Math.random() * 6) + 1;
-  //2. display reasult
-  let diceDOM = document.querySelector(".dice");
-  diceDOM.style.display = "block";
-  diceDOM.src = "dice-" + dice + ".png";
-  // dice-2.png
+  if (gamePlay) {
+    //1. ramdon number
+    let dice = Math.floor(Math.random() * 6) + 1;
+    //2. display reasult
+    let diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
 
-  //3. update the round score but lony if the rolled number was not a 1
-  if (dice !== 1) {
-    //add score
-    //this does the same as roundScore += dice;
-    //roundScore = roundScore + dice;
-    roundScore += dice; //this update all internal variable value
-    document.querySelector("#current-" + activePlayer).textContent = roundScore; //this display the apdated value to the user interface
-  } else {
-    //**This call back function call the next player
-    nextPlayer();
+    //3. update the round score but lony if the rolled number was not a 1
+    if (dice !== 1) {
+      //add score
+      //this does the same as roundScore += dice;
+      //roundScore = roundScore + dice;
+      roundScore += dice; //this update all internal variable value
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore; //this display the apdated value to the user interface
+    } else {
+      //**This call back function call the next player
+      nextPlayer();
+    }
   }
 });
 
 //
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  // 1. Add current score to the blobal score
-  //scores array access activePlayer to manipulate it
-  // scores[activePlayer] = scores[activePlayer] + roundScore; // this code does the same thing as the code below
-  scores[activePlayer] += roundScore;
+  if (gamePlay) {
+    // 1. Add current score to the blobal score
+    //scores array access activePlayer to manipulate it
+    // scores[activePlayer] = scores[activePlayer] + roundScore; // this code does the same thing as the code below
+    scores[activePlayer] += roundScore;
 
-  // 2. update th euser interface
-  document.querySelector("#score-" + activePlayer).textContent =
-    scores[activePlayer];
+    // 2. update th euser interface
+    document.querySelector("#score-" + activePlayer).textContent =
+      scores[activePlayer];
 
-  // 3. check if the player won the game, score 100 >
-  if (scores[activePlayer] >= 20) {
-    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-    document.querySelector(".dice").style.display = "none";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
-  } else {
-    //next player call back function
-    nextPlayer();
+    // 3. check if the player score 100 or more, won the game! >
+    if (scores[activePlayer] >= 100) {
+      document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+      document.querySelector(".dice").style.display = "none";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+
+      // disable the the hold button
+      gamePlay = false;
+    } else {
+      //next player call back function
+      nextPlayer();
+    }
   }
 });
 
@@ -112,4 +115,27 @@ function nextPlayer() {
   document.querySelector(".dice").style.display = "none";
 
   // document.querySelector(".dice").style.animationDuration = "2s";
+}
+
+document.querySelector(".btn-new").addEventListener("click", initFunction);
+
+// the porpuse of this function is to not repet the same code over and over
+function initFunction() {
+  scores = [0, 0];
+  activePlayer = 0;
+  roundScore = 0;
+  gamePlay = true;
+
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.querySelector(".player-0-panel").classList.add("active");
 }
